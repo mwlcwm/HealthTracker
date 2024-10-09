2f0d7665d996eaef1b9ba9cac2728a042c4cbd39
Just1passingby <fanguinoveny.aurore@gmail.com>
2024-10-09 11:45:07 +0200

    [ADD] - front

--- mytracker/src/App.js
+++ mytracker/src/App.js
@@ -1,26 +1,16 @@
 import './App.css';
+import { Link, Route, BrowserRouter, Routes } from "react-router-dom";
+import Journal from './Journal';
+import Entry from './Entry';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-      <img src="Health tra.png" id="logo"/>
-      </header>
-      <h1 class="page_title">Journal</h1>
-      <div  id="tracker">
-        <div class="entry">
-            <h2>Placeholder</h2>
-            <img class="intensity" alt="intensity"/>
-            <span class="reason">Motif:</span>
-            <span id="date">À: </span>
-            <p>This is my issue</p>
-            <button class="edit">Modifier</button>
-        </div>
-      </div>
-
-      <button class="add">+</button>
-
-      </div>
+    <BrowserRouter>
+    <Routes>
+      <Route exact path="/Journal" element={<Journal />} />
+      <Route path="/Entry" element={<Entry />} />
+    </Routes>
+  </BrowserRouter>
   );
 }
 
--- mytracker/src/index.js
+++ mytracker/src/index.js
@@ -2,7 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import Entry from './Entry'; 
 import reportWebVitals from './reportWebVitals';
+import { Link, Route, BrowserRouter as Router } from "react-router-dom";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
